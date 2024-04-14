9d53ff0aaaef83b1dcf2576a830cdd4954df2e80
Antoine ZEBAZE <zeba_antoine@yahoo.fr>
2024-04-14 09:25:41 +0200

    Modification nom de la base de donnée en messagerieInstantanee

--- lib/pdo.php
+++ lib/pdo.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=messagerieinstantannee;', 'root', '');
+    $pdo = new PDO('mysql:host=localhost;dbname=messagerieInstantanee;', 'root', '');
 } catch (PDOException $e) {
     echo 'Erreur de connexion à la Base de données' . $e->getMessage();
 }
